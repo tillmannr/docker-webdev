65dd00484b0867a8063a5ec65aa9d8d970238395
tillmannr <robert@tillmann.hu>
2017-03-05 20:17:23 +0100

    fixed php mysql connection issue

--- code/index.php
+++ code/index.php
@@ -7,17 +7,13 @@ $app = new Silex\Application();
 
 $app['debug'] = true;
 $app->get('/blog', function() use($app) {
-//	var_dump(php_ini_loaded_file());
-//	var_dump(get_loaded_extensions());
-//	var_dump(extension_loaded('pdo_mysql'));
-//	print phpinfo();
 
 	$config = new \Doctrine\DBAL\Configuration();
 	$connectionParams = array(
 		'dbname' => 'myapp',
 		'user' => 'root',
 		'password' => '123456',
-		'host' => 'localhost',
+		'host' => 'db',
 		'driver' => 'pdo_mysql',
 	);
 	$conn = \Doctrine\DBAL\DriverManager::getConnection($connectionParams, $config);
@@ -29,13 +25,7 @@ $app->get('/blog', function() use($app) {
 		echo print_r($row);
 	}
 
-//
-//	$conn = new PDO('mysql:host=localhost;dbname=myapp', 'root', '123456');
-//	if ($conn->connect_error) {
-//		die("Connection failed: " . $conn->connect_error);
-//	}
-
-	return;
+	return true;
 });
 
 $app->run();
